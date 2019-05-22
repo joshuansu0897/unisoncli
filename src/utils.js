66310286b7ac99b33e827a23e86021697fb926aa
Joshua Saucedo <joshuansu0897@gmail.com>
2019-05-21 18:55:37 -0700

    add encrypt and decrypt

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,21 @@
-function encrypt(str) {
+const configstore = require('configstore')
 
+const Cryptr = require('cryptr');
+const pkg = require('../package.json')
+const conf = new configstore(pkg.name)
+
+if (!conf.has('scr')) {
+  conf.set('scr', require('crypto').randomBytes(256).toString('hex'))
 }
 
-function decrypt(str) {
+const cryptr = new Cryptr(conf.set('scr'));
 
+function encrypt(text) {
+  return cryptr.encrypt(text)
+}
+
+function decrypt(text) {
+  return cryptr.decrypt(text)
 }
 
 module.exports = {
